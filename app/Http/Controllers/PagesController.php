127127d2e160aa99f6162b2ebd82a110d2669c06
stivipp <stivippx@gmail.com>
2021-01-02 14:47:01 +0100

    Learning and coding about ROUTES/web.php & RESOURCES/VIEWS/welcome.blade.php & APP/HTTP/CONTROLLEERS/PagesController.php + little bit of TAILWIND styling adding :)

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home() 
+    {
+        return view('welcome', [
+            'title' => '(@_@)',
+            'posts' => [
+                'Hi man I am the best one',
+                'And you are too the best :D',
+                'Paul I Atreides form Arrakis',
+                'Harkonnens are Rich bitches',
+            ]
+        ]);
+    }
+
+    
+    public function about() 
+    {
+        return "about page";
+    }
+
+    
+    public function contact() 
+    {
+        return "contact page";
+    }
+}
