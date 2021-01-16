1727b314d48ae17e3c72801c49896d176097ed7e
stivipp <stivippx@gmail.com>
2021-01-16 12:28:22 +0100

    Styling LOGIN & REGISTER Form using TAILWIND css framework.

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+{{-- <section class="max-w-screen-2xl mx-auto bg-gray-800 rounded-t-2xl shadow-xl">
+
+    <div class="p-4">
+         <p class="text-yellow-200  text-center ">This is footer</p> 
+    </div>
+    
+
+</section>--}}
